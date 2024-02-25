6fa16bb822f045167878b7050fd111ee820a33a3
huzaifa157 <m.huzaifa157@gmail.com>
2024-02-25 16:19:12 +0500

    new

--- script.js
+++ script.js
@@ -21,92 +21,6 @@ if (close) {
 
 // cart 
 
-
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
-80
-81
-82
-83
-84
 const product = [
    {
        id: 0,
@@ -191,4 +105,58 @@ function displaycart(){
 
    
 }
+const shop = document.getElementById("prepend");
 
+const add = document.getElementById("add");
+add.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    //input
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+    const fileInput = document.getElementById('file').value;
+    // const file = fileInput.files[0];
+  
+    // const reader = new FileReader();
+  
+    // reader.onload = function(event) {
+    //   const contents = event.target.result;
+    //   displayContents(contents);
+    // };
+  
+    // reader.onerror = function() {
+    //   console.error('Error reading the file');
+    // };
+  
+    // reader.readAsText(file);
+
+    let el = document.createElement("div");
+    el.setAttribute("class", "pro");
+    el.innerHTML = `<a href="products.html">${fileInput}</a>
+    <div class="des">
+        <span>Hideaway</span>
+        <h5>${name}</h5>
+        <div class="star">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+
+        </div>
+        <h4>${price}</h4>
+    </div>
+    <a href="products.html"><i class="fa fa-shopping-cart cart"></i></a>`
+
+    adding(el);
+    
+})
+
+  function displayContents(contents) {
+    const element = document.getElementById('fileContents');
+    element.textContent = contents;
+  }
+
+  function adding(el){
+    shop.prepend(el);
+  }
